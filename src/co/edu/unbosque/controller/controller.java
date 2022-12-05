db2d875bf543825086106013f821e3868de836eb
jota7337 <jyate@unbosque.edu.co>
2022-12-04 19:55:47 -0500

    Codigo basico

--- src/co/edu/unbosque/controller/controller.java
+++ src/co/edu/unbosque/controller/controller.java
@@ -1,5 +1,50 @@
 package co.edu.unbosque.controller;
 
-public class controller {
+import co.edu.unbosque.view.view;
 
+public class controller {
+	private view vista;
+	
+	
+	public controller() {
+		vista= new view();
+		
+		funcionar();
+	}
+	public void funcionar() {
+
+		
+
+	
+		while(vista.getSelect()!=0) {
+			int numeroMenu = vista.leerDatoMenu();
+			vista.setSelect(numeroMenu);
+			
+			switch(vista.getSelect()) {
+			case 1:
+				
+	
+				
+				
+				
+		         
+		
+				break;  
+			case 2:
+			
+			
+				break;  
+		
+	
+			case 0:
+			 
+				break;
+			default:
+
+				break;
+			}
+		}
+		
+		
+	}
 }
