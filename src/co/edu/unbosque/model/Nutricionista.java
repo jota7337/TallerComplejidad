072affeb7ce88e4ac6481e4abe489c0ba89d6dc9
jota7337 <jyate@unbosque.edu.co>
2022-12-06 02:45:24 -0500

    1 termimado

--- src/co/edu/unbosque/model/Nutricionista.java
+++ src/co/edu/unbosque/model/Nutricionista.java
@@ -3,12 +3,11 @@ package co.edu.unbosque.model;
 import java.util.Arrays;
 
 public class Nutricionista {
-	
-	public String dp(int calories [], int calorie){
-		if(sum(calories) < calorie) return "No se puede realizar el calculo.";
-		Arrays.toString(calories);
+	public String optimoMenu(int calories [], int calorie){
+		Arrays.sort(calories);
 		int nums [][] = new int [calories.length +1][calorie +1];
 		StringBuilder chars [][] = new StringBuilder [nums.length][nums[0].length];
+		String values [] = values(calories.length);
 		for(int i = 1; i < nums[0].length; i++)
 			nums[0][i] = Integer.MAX_VALUE;
 		for(int i = 1, compare; i < nums.length; i++) {
@@ -16,19 +15,20 @@ public class Nutricionista {
 				StringBuilder builder = new StringBuilder();
 				if(calories[i - 1] <= j) {
 					compare = nums[i - 1][j - calories[i - 1]];
-					builder.append(compare != 0? chars[i -1][j - calories[i -1]] : "");
+					builder.append(compare != 0? chars[i - 1][j - calories[i - 1]] : "");
 					if(compare < Integer.MAX_VALUE) {
-						builder.append(calories[i - 1]);
+						builder.append(imprimir(values, calories, calories[i - 1]));
 						compare += calories[i - 1];
 					}
 				}else {
 					compare = calories[i - 1];
-					builder.append(compare);
+					builder.append(imprimir(values, calories, compare));
 				}
 				if(compare >= j) {
 					if(nums[i -1][j] < compare) {
 						builder.setLength(0);
-						builder.append(chars[i - 1][j]);
+						String value = imprimir(values, calories, nums[i - 1][j]);
+						builder.append(value == ""? chars[i - 1][j] : value);
 					}
 				}
 				nums[i][j] = compare >= j? Math.min(nums[i - 1][j], compare) : Integer.MAX_VALUE;
@@ -37,15 +37,33 @@ public class Nutricionista {
 		}
 		String fin = chars[calories.length][calorie].toString();
 		String caloriasOptimas = "Las calorias optimas son: " + nums[calories.length][calorie] + "\n";
-		for(int i = 0; i < fin.length(); i++) 
-			caloriasOptimas += "Plato " + ((char) (65 + i)) + " proteinas = " + (fin.charAt(i)) + "\n"; 
+		for(int i = 0; i < fin.length(); i++) {
+			int optimas = valueInt(values, calories, String.valueOf(fin.charAt(i)));
+			caloriasOptimas += "Plato " + imprimir(values,calories,optimas) + " calorias: " + optimas + "\n";
+		}
 		return caloriasOptimas;
 	}
 	
-	private int sum(int calorias []) {
-		int sum = 0;
-		for(int i = 0; i < calorias.length; i++)
-			sum += calorias[i];
-		return sum;
+	
+	private int valueInt(String chars [], int calories [], String letter) {
+		//System.out.println("Chars = " + Arrays.toString(chars) + ", calories = " + Arrays.toString(calories) + "Letter = " + letter);
+		for(int i = 0; i < calories.length; i++)
+			if(chars[i].equalsIgnoreCase(letter))
+				return calories[i];
+		return -1;
+	}
+
+	private String imprimir(String letter [], int calories [], int value) {
+		for(int i = 0; i < calories.length; i++)
+			if(calories[i] == value) 
+				return letter[i];
+		return "";
+	}
+	
+	private String [] values(int length) {
+		String chars [] = new String [length];
+		for(int i = 0; i < chars.length; i++)
+			chars[i] = String.valueOf((char)(65 + i));
+		return chars;
 	}
 }
